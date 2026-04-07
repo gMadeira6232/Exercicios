70006412880021c280151e760eb4054958db67fe
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-06 22:30:19 -0300

    Calculadora de expressao com aplicaçao de if

--- src/ExpressaoComIf.java
+++ src/ExpressaoComIf.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ExpressaoComIf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double x, y, xQuadrado;
+
+        System.out.println("Informe o valor de X: ");
+        x = sc.nextDouble();
+
+        if (x > 5 || x < -5) {
+            y = 8 / Math.sqrt(x * x - 25);
+            System.out.println("O valor de y é: " + y);
+        } else {
+            System.out.println("Insira um valor válido");
+        }
+    }
 }
